3e7e664804620e7e09abbb0038afe01b4701bed4
dulltz <isrgnoe@gmail.com>
2020-01-12 19:42:51 +0900

    Fix to use generic options

--- cmd/kubectl-megaconfigmap/main.go
+++ cmd/kubectl-megaconfigmap/main.go
@@ -12,10 +12,12 @@ func main() {
 	flags := pflag.NewFlagSet("kubectl-megaconfigmap", pflag.ExitOnError)
 	pflag.CommandLine = flags
 
-	root, err := megaconfigmap.NewCmdMegaConfigMap(genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr})
+	streams := genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr}
+	root, err := megaconfigmap.NewCmdMegaConfigMap(streams)
 	if err != nil {
 		panic(err)
 	}
+	root.AddCommand(megaconfigmap.NewCmdCreate(streams))
 	if err := root.Execute(); err != nil {
 		os.Exit(1)
 	}
